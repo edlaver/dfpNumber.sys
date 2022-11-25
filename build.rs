d58222f3a7bc0c25d4fc9d1467147d4f0f0b5838
senees <23065216+senees@users.noreply.github.com>
2022-11-25 16:35:23 +0100

    Supressed warnings.

--- build.rs
+++ build.rs
@@ -64,6 +64,7 @@ fn main() {
     .flag_if_supported("-Wno-comment")
     .flag_if_supported("-Wno-parentheses")
     .flag_if_supported("-Wno-maybe-uninitialized")
+    .flag_if_supported("-Wno-array-bounds")
     .file("IntelRDFPMathLib20U2/LIBRARY/float128/dpml_exception.c")
     .file("IntelRDFPMathLib20U2/LIBRARY/float128/dpml_four_over_pi.c")
     .file("IntelRDFPMathLib20U2/LIBRARY/float128/dpml_ux_bessel.c")
